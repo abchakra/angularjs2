0536bf667a42abb2025c2b44762006cefa695646
Abhishek Chakraborty <chakraborty@silexica.com>
2016-06-22 22:25:56 +0200

    Completed chapter5 with routing v2

---
+++ angular2-tour-of-heroes/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+ { id: 11, name: 'Superman' },
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'Ironman' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Spiderman' },
+  { id: 16, name: 'Antman' },
+  { id: 17, name: 'Black Widow' },
+  { id: 18, name: 'Hulk' },
+  { id: 19, name: 'Thor' },
+  { id: 20, name: 'Tornado' }
+];
